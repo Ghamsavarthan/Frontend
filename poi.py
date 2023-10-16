eb2f9254e11b2a66f647f855684118a39fb56c0b
Ghamsavarthan <gopahanhamsavarthan@gmail.com>
2023-10-17 01:29:01 +0530

    Adding new python files

---
+++ poi.py
@@ -0,0 +1,10 @@
+import random
+
+num_1 = int()
+num_2 = int()
+
+if(num_1 <= num_2):
+    print("Your second number is greater than first number")
+else:
+    print("Your first number is greater than second number")
+    
